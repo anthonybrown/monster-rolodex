9605a1e9ae2c44ec1090d0bad732fe7667c8d5d2
Tony Brown <tony@tonybrown.me>
2019-09-19 04:14:43 -0400

    Updates

---
+++ precache-manifest.77522065d7d4cb8dde02e082fa339315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13316c92e221a88b87cef5252568e5f6",
+    "url": "/monsters-rolordex/index.html"
+  },
+  {
+    "revision": "b8810c651ba48bd5c0b3",
+    "url": "/monsters-rolordex/static/css/main.be005ac0.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolordex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "b8810c651ba48bd5c0b3",
+    "url": "/monsters-rolordex/static/js/main.f18ad794.chunk.js"
+  },
+  {
+    "revision": "2587eaaeb5ca1eb924fd",
+    "url": "/monsters-rolordex/static/js/runtime~main.9a66984e.js"
+  }
+]);
